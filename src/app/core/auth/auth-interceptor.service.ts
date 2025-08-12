8865f7a68af56edcd4e8bb82bf5c5cdbebf2dc48
Jacobsin12 <tristanjacob755@gmail.com>
2025-08-12 09:29:11 -0600

    Modificacion del interceptor intento de arreglar el otp

--- src/app/core/auth/auth-interceptor.service.ts
+++ src/app/core/auth/auth-interceptor.service.ts
@@ -3,9 +3,9 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Interceptor - URL antes:', req.url);
 
-  const token = localStorage.getItem('token'); // Puede ser temporal o final
+  const token = localStorage.getItem('token');
 
-  if (token) {
+  if (token && !req.url.includes('/auth/verify-otp')) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
