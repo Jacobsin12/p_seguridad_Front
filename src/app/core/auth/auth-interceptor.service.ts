b92be8f0dcd230ae19fa30a9019adec83b453dbd
Jacobsin12 <tristanjacob755@gmail.com>
2025-08-12 12:01:28 -0600

    provando el interceptor2

--- src/app/core/auth/auth-interceptor.service.ts
+++ src/app/core/auth/auth-interceptor.service.ts
@@ -4,8 +4,9 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Interceptor - URL antes:', req.url);
 
   const token = localStorage.getItem('token');
-//hola esto es una prueba 
-  if (token && !req.url.includes('/auth/verify-otp')) {
+
+  // Aquí excluimos la ruta exacta /verify-otp
+  if (token && !req.url.endsWith('/verify-otp')) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
